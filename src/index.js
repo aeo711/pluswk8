269758f7efc8cc8c464ee8c834164f4d0f9a5ec8
aeo711 <adreaojukwu@gmail.com>
2025-03-05 20:07:52 -0600

    Debugging the JS displayForecast Function
    All of the code is correct. It may be a browser issue.

--- src/index.js
+++ src/index.js
@@ -59,8 +59,6 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-
   let forecastHTML = "";
 
   response.data.daily.forEach(function (day, index) {
